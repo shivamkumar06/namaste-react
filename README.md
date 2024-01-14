5be679170e23f9bc7d224fe1af044e6741b16987
shivamkumar06 <shivam.saha06@gmail.com>
2024-01-14 19:43:23 +0530

    episode-05

--- README.md
+++ README.md
@@ -20,3 +20,35 @@
 - HTTPs
 - Tree Shaking - remove unused codes
 - Different dev and prod bundles
+
+
+
+
+# Namaste Food
+
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - Restaurant Container
+ *    - RestaurantCard
+ *        - Img
+ *        - Name of Rest, Star Rating, cuisine, delivery time
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+
+ Two Types of Export/Import
+
+ - Default Export/Import
+ export default Component;
+ import Component from "path";
+
+ - Named Export/Import
+ export const Component;
+ import {Component} from "path";
---
+++ src/components/Body.js
@@ -0,0 +1,32 @@
+
+import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+import resList from "../utils/mockData";
+
+const Body = () => {
+
+    // Local State Variable - Super powerful variable
+    const [listOfRestaurants, setListOfRestaurant] = useState(resList);
+
+    // Normal JS Variable
+    // let listOfRestaurants = [];
+    return (
+      <div className="body">
+        <div className="filter">
+            <button className="filter-btn" onClick={()=>{
+                  const filteredList = listOfRestaurants.filter(
+                    (restaurant)=> restaurant.data.avgRating > 4);  
+                    setListOfRestaurant(filteredList);        
+            }} >Top Rated Restaurants</button>
+        </div>
+        <div className="res-container">
+          {
+            listOfRestaurants.map((restaurant)=> (
+            <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
+            ))}
+        </div>
+      </div>
+    );
+  };
+
+    export default Body;
